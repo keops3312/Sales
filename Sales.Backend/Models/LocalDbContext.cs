96ce0f0b301e2af52caeeef404ac586222aa5534
Felipe Reyes <keops_3312@hotmail.com>
2018-10-23 13:35:00 -0500

    Architecture MVC-Common-Domain-APi

---
+++ Sales.Backend/Models/LocalDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Sales.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sales.Backend.Models
+{
+    public class LocalDbContext:DataContext
+    {
+    }
+}
---
+++ Sales.Common/Models/Response.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Common.Models
+{
+    public class Response
+    {
+        public bool IsSucces { get; set; } //si la respuesta fue exitosa o no
+        public string Message { get; set; }// el mensaje de respuesta
+
+        public object Result { get; set; }//para que devuelva cualquier objeto
+    }
+
+}
